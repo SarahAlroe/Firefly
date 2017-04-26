403fb462ccac9968a8a60ee4855a11d0b0e85ed2
tobalr <tobiasalroe@gmail.com>
2017-04-26 20:57:31 +0000

    fly behaviour blink more random. Less bright and slower pusling bugs

--- Firefly.py
+++ Firefly.py
@@ -12,7 +12,7 @@ LED_COUNT      = 6             # Number of LED pixels.
 LED_PIN        = 18            # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000        # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5             # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 255           # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 128           # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False         # True to invert the signal (when using NPN transistor level shift)
 FLY_PINS       = [16,22,32,36] # Pins on which flies are connected.
 
